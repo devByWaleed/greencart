018cc6801954e26eaf5341800cba926d5c0de99a
waleed1857 <waleed.cool1857@gmail.com>
2026-01-22 18:29:50 +0500

    Create Seller Dashboard

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -23,15 +23,45 @@ const MyOrders = () => {
                 <div className='w-16 h-0.5 bg-primary rounded-full'></div>
             </div>
 
-            {myOrders.map((order, index) => {
+            {myOrders.map((order, index) => (
                 <div key={index} className='border border-gray-300 rounded-lg mb-10 p-4 py-5 max-w-4xl'>
                     <p className='flex justify-between md:items-center text-gray-400 md:font-medium max-md:flex-col'>
                         <span>OrderId : {order._id}</span>
                         <span>Payment : {order.paymentType}</span>
                         <span>Total Amount : {currency}{order.amount}</span>
                     </p>
+
+                    {order.items.map((item, index) => (
+                        <div key={index}
+                        className={`relative bg-white text-gray-500/70 ${
+                            order.items.length !== (index + 1) && 'border-b'
+                        } border-gray-300 flex flex-col md:flex-row md:items-center justify-between p-4 py-5 md:gap-16 w-full max-w-4xl`}>
+                            
+                            <div className='flex items-center mb-4 md:mb-0'>
+                                <div className='rounded-lg bg-primary/10'>
+                                    <img src={item.product.image[0]} alt="Product" className='w-16 h-16' />
+                                </div>
+
+                                <div className='ml-4'>
+                                    <h2 className='text-xl font-medium text-gray-800'>{item.product.name}</h2>
+                                    <p className=''>{item.product.category}</p>
+                                </div>
+                            </div>
+
+                            <div className='flex flex-col justify-center md:ml-8 mb-4 md:mb-0'>
+                                <p>Quantity : {item.quantity || "1"}</p>
+                                <p>Status : {item.status}</p>
+                                <p>Date : {new Date(order.createdAt).toLocaleDateString()}</p>
+                            </div>
+                            <p className='text-primary text-lg font-medium'>
+                                Amount: {currency}{item.product.offerPrice * item.quantity}
+                            </p>
+
+
+                        </div>
+                    ))}
                 </div>
-            })}
+            ))}
         </div>
     )
 }
